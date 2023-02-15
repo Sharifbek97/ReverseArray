cff19c781701522a18e08da03d9844bcfac1305d
Sharifbek97 <sharifbekmirzozoda2@gmail.com>
2023-02-15 21:45:54 +0500

    Added reverse array

---
+++ OnTheContraryArray/Program.cs
@@ -0,0 +1,14 @@
+﻿var numbers= new []{3,6,12,-1};
+var count = numbers.Length;
+ for (var i = 0; i < count ; ++i)
+ {
+     Console.Write(numbers[i]+"   ");
+ }
+ Console.Write("\n");
+var array= new int[count];
+Console.Write("\tGiven elements of newarray: ");
+for (var j = 0; j < count; ++j)
+{
+    array[j] = numbers[count - 1 - j];
+    Console.Write(array[j] + "   ");
+}

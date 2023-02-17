428f12201ee5807feab1b0a9217854502293da16
Sharifbek97 <sharifbekmirzozoda2@gmail.com>
2023-02-17 19:41:47 +0500

    Change for with foreach in the reverse array

--- OnTheContraryArray/Program.cs
+++ OnTheContraryArray/Program.cs
@@ -1,14 +1,16 @@
 ﻿var numbers= new []{3,6,12,-1};
 var count = numbers.Length;
- for (var i = 0; i < count ; ++i)
+ foreach (var i in numbers)
  {
-     Console.Write(numbers[i]+"   ");
+     Console.Write(i+"   ");
  }
  Console.Write("\n");
 var array= new int[count];
 Console.Write("\tGiven elements of newarray: ");
-for (var j = 0; j < count; ++j)
+int n = 0;
+foreach (var j in array)
 {
-    array[j] = numbers[count - 1 - j];
-    Console.Write(array[j] + "   ");
+    array[j] = numbers[count - 1 - n];
+    Console.Write( array[j]+ "   ");
+    ++n;
 }
